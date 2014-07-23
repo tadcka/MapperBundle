b9c4bbebea566322bd6b57091db1befe35e74e0e
Tadas <tadcka89@gmail.com>
2014-07-24 00:47:34 +0300

    Add cache manager.

---
+++ Tests/Cache/CacheManagerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+ * This file is part of the Tadcka package.
+ *
+ * (c) Tadcka <tadcka89@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tadcka\Bundle\MapperBundle\Tests\Cache;
+
+/**
+ * @author Tadas Gliaubicas <tadcka89@gmail.com>
+ *
+ * @since 7/24/14 12:19 AM
+ */
+class CacheManagerTest 
+{
+
+}
+ 
